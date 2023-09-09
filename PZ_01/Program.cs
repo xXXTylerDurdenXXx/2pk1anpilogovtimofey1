6c08acb388f1109642fa1dd4bfab046985c46612
Tyler Durden <Tyler Durden@DESKTOP-U6SHDSV>
2023-09-09 19:38:48 +0300

    Добавьте файлы проекта.

---
+++ PZ_01/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PZ_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("введите число a");
+            double a =double.Parse( Console.ReadLine());
+            if (a <= 0 )
+            {
+                Console.WriteLine("Перездайте число недопустимое значение ");
+                 a  = double.Parse( Console.ReadLine());
+            }
+            
+
+            Console.WriteLine("введите число c");
+            double c =double.Parse( Console.ReadLine());
+            Console.WriteLine("введите число b");
+            double b = double.Parse( Console.ReadLine());
+            double value1, value2, value3, value4, value5;     // Задаем переменные
+          
+            // разделяем пример на дествиия
+            value1 = (3 * (Math.Log(8, a)));                   // действие 1    
+           
+         value2 = ( Math.Sin(b / (3 * a)));                    // действие 2
+
+            value3 = Math.Cbrt(5 * (Math.Pow(a, 2)) + 7);      // действие 3
+            value4 = 4 *  Math.Abs(c - (2 * a) + 1);          // действие 4
+            value5 = Math.Sqrt(8*a);                          // действие 5
+            double result = (value1 / value2) - (value3 ) + (value4 / value5);   // складываем все результаты
+           double finish =  Math.Round(result, 0 );           // округляем
+
+            Console.WriteLine("Ответ =  " + finish);                        //выводим результат на консоль
+
+
+
+
+            Console.ReadLine();
+
+          
+
+
+        }
+    }
+    
+}
