74523b722bf5b7e2d57c9b225609d08b3a3d093c
Тимофей Анпилогов <timofejanpilogov356@gmail.com>
2023-09-22 23:07:52 +0300

    Добавил 5 практику

---
+++ PZ_05/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество км для первого дня");
+            double x = double.Parse(Console.ReadLine());    
+            Console.WriteLine("Введите количество конечного результата");
+            double y = double.Parse(Console.ReadLine());
+            double prozent = 1.1;
+            int countDays = 1;
+            do  
+            {
+                Console.WriteLine($"День: {countDays}" + $"  Пробег: {x}");
+                x =( x * prozent);
+                x = Math.Round(x,1);  
+                countDays++;
+               
+
+            } while (x <=  y);
+            Console.WriteLine($"Номер дня: {countDays}");
+        }
+    }
+}
