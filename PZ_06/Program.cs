de9d9b8cdbea5a46505b9e4a247c2285df5d34b1
Тимофей Анпилогов <timofejanpilogov356@gmail.com>
2023-09-30 09:25:59 +0300

    добавил практики

---
+++ PZ_06/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Random random = new Random();
+            int[] myArray= new int[50]; // Задаем первый массив и его длинну
+            int length = 0;             // Счетчик для определения длинны второго массива
+            int count = 0;             // Счетчик 
+            for (int i = 0; i < myArray.Length; i++)  // Пересчет масива 
+            {
+                myArray[i] = random.Next(-101, 101);  // задаем значения через рандом
+                Console.WriteLine(myArray[i]);  // выводим  на консоль
+
+                if (myArray[i] >= -10 && myArray[i] <= 10)   // условия для определения длинны второго массива
+                {
+                    length++;
+                }
+
+            }
+            int[] youArray = new int[length];  // Задаем второй массив и его длинну
+
+            for (int i = 0; i < myArray.Length; i++)  //Пересчет массива
+            {
+
+
+                if (myArray[i] >= -10 && myArray[i] <= 10) //  Условия для заполнения второго массива
+                {
+                    youArray[count] = myArray[i];
+                    count++;
+                }       
+                
+
+            }
+            Console.WriteLine("Вывод второго массива "); // соответссвенно вывод второго массива
+            for (int i = 0; i < youArray.Length; i++)
+            {
+                Console.WriteLine(youArray[i]);
+
+            }
+
+        }
+    }
+}
---
+++ PZ_07/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PZ_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] arr = new char[8, 6]; //  Дан массив по условию
+            int hight = arr.GetLength(0);
+            int wight = arr.GetLength(1);
+            char start = 'a';
+            for (int x = 0; x < hight;  x++)
+            {
+               for (int y = 0; y < wight; y++) // Задаем и выводим исходный массив
+                {
+                    arr[x, y] = start;
+                    start++;
+                    Console.Write(arr[x,y]);
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine(arr.GetLength(0));
+            
+
+
+        }
+    }
+}
