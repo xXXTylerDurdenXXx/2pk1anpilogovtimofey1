b867afe6bf4cab9ca49db77eecd0cf5f780f4646
Тимофей Анпилогов <timofejanpilogov356@gmail.com>
2023-12-01 10:51:11 +0300

    практика 12

---
+++ PZ_12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine(); // вводим строку
+
+            double r = Str(str);
+            
+            Console.WriteLine(($"Процент символов пунктуации: {r}% ")); //Вывод на консоль
+        }
+        static  double Str(string str)
+        {
+            int count = 0;  
+            //Создаем цикл который который прогоняет строку на проверку наличия пунктуационных символов
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsPunctuation(str[i]))
+                {
+                    count++; //Заполняем счетчик при выполнении условия
+                }
+            }
+
+
+            double result = ((double)count / str.Length) * 100.0 ; // Выводим процентное соотношение
+            
+            return result ;
+        }
+    }
+}
