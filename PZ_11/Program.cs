9558722d6cb7cc87e1279e82b152b79d6dd1a2ec
Тимофей Анпилогов <timofejanpilogov356@gmail.com>
2023-12-01 07:22:47 +0300

    практика 11

---
+++ PZ_11/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пределы");
+            Console.WriteLine("Введите минимальный предел");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальный предел");
+            int n = int.Parse(Console.ReadLine());
+           int summ;
+            GetSquareSumm(m, n,out summ);
+            Console.WriteLine(summ);
+        }
+        static void GetSquareSumm(int m ,int n, out int summ)
+        {
+             summ = 0;
+            for (int i = m; i < n; i++)
+            {
+                summ += i * i;
+            }
+            
+        }
+
+    }
+}
